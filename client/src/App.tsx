fc22c603409a6ffbf116017a952b557a571882ec
sivabalanedu1@gmail.com <sivabalanedu1@gmail.com>
2026-09-24 05:44:21 +0530

    chore: configure production deployment

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,25 +4,25 @@ import Login from "./pages/Login";
 import Layout from "./components/Layout";
 import Dashboard from "./pages/Dashboard";
 import Accounts from "./pages/Accounts";
-import AIComposer from "./pages/AIComposer";
 import Scheduler from "./pages/Scheduler";
+import AIComposer from "./pages/AIComposer";
 import { Toaster } from "react-hot-toast";
-export default function App() {
-  return (
-    <>
-    <Toaster position="top-right"/>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route element={<Layout />}>
 
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/accounts" element={<Accounts />} />
-          <Route path="/ai-composer" element={<AIComposer />} />
-          <Route path="/scheduler" element={< Scheduler/>} />
-          
-        </Route>
-      </Routes>
-    </>
-  );
+export default function App() {
+    return (
+        <>
+            <Toaster position="top-right"/>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/login" element={<Login />} />
+                <Route element={<Layout />}>
+                    <Route path="/dashboard" element={<Dashboard />}/>
+                    <Route path="/accounts" element={<Accounts />}/>
+                    <Route path="/scheduler" element={<Scheduler />}/>
+                    <Route path="/ai-composer" element={<AIComposer />}/>
+
+                </Route>
+            </Routes>
+        </>
+    );
 }
